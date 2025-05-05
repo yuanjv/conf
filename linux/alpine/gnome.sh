bdc9befd80e08d68ab953e26e53c43bb2cc10b45
Nathan Wu <nianzhew@uci.edu>
2025-05-05 15:20:44 -0700

    update

--- linux/alpine/gnome.sh
+++ linux/alpine/gnome.sh
@@ -1,8 +1,10 @@
 apk update && apk upgrade
-setup-desktop gnome (as said by https://wiki.alpinelinux.org/wiki/GNOME )
+setup-desktop gnome #(as said by https://wiki.alpinelinux.org/wiki/GNOME )
 #del unwanted softwares (gnome-software etc.)
-#---
+
+
+
 #get wifi gui working (use networkmanager)
-https://wiki.alpinelinux.org/wiki/NetworkManager
+#https://wiki.alpinelinux.org/wiki/NetworkManager
 
 
